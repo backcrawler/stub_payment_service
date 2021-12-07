0b0c67e86a15787a3c8bac0dfcbd832cb5fa71b4
backcrawler <n-lukash@list.ru>
2021-12-07 15:53:22 +0300

    Context DB Manger, Neg validation

--- stub_payment_service/balance_repo.py
+++ stub_payment_service/balance_repo.py
@@ -1,47 +1,60 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 from asyncpg.exceptions import UniqueViolationError
 
-from .db_service import get_db_connection
+from .db_service import DBConnectionContext
 
 
 async def peer_balance(user_id: str, amount: int, idempotency_key: Optional[str]):
     if not idempotency_key:
-        return {'result': 'failed', 'reason': 'no idempontency key provided'}
-
-    conn = await get_db_connection()
+        return {'result': 'failed', 'reason': 'no idempotency key provided'}
 
     try:
-        async with conn.transaction():
+        async with DBConnectionContext() as conn:
+            trans = conn.transaction()  # ¯\_(ツ)_/¯
             try:
+                try:
+                    await conn.fetch(
+                        f'''INSERT INTO paymentKey (userId, externalKey) VALUES ('{user_id}', '{idempotency_key}')'''
+                    )
+                except UniqueViolationError as exc:
+                    print(f'Index error occurred: {exc}; {type(exc)}')
+                    return {'result': 'success'}
+                except Exception as exc:
+                    print(f'Error occurred: {exc}')
+                    # await trans.rollback()
+                    return {'result': 'failed', 'reason': 'internal error'}
+
+                check_status, return_result = await check_current_balance(conn, user_id, amount)
+                if not check_status:
+                    return return_result
+
                 await conn.fetch(
-                    f'''INSERT INTO paymentKey (userId, externalKey) VALUES ('{user_id}', '{idempotency_key}')'''
+                    f'''INSERT INTO customer (userId, balance) VALUES ('{user_id}', {amount})
+                    ON CONFLICT (userId) DO UPDATE SET balance = customer.balance + {amount}
+                    '''
                 )
-            except UniqueViolationError as exc:
-                print(f'Index error occured: {exc}; {type(exc)}')
-                return {'result': 'success'}  # todo: change
             except Exception as exc:
-                print(f'Error occured: {exc}')
-                return {'result': 'failed', 'reason': 'internal error'}
-
-            if amount < 0:
-                already_exists = await conn.fetch(f'''SELECT COUNT(*) FROM customer WHERE userid = '{user_id}\'''')
-                if not already_exists:
-                    print(f'Cannot withdrew with no account')
-                    return {'result': 'failed', 'reason': 'account does not exist'}  # todo: change
-
-            await conn.fetch(
-                f'''INSERT INTO customer (userId, balance) VALUES ('{user_id}', {amount})
-                ON CONFLICT (userId) DO UPDATE SET balance = customer.balance + {amount}
-                '''
-            )
+                await trans.rollback()
+                raise exc
 
     except Exception as exc:
-        print(f'Error occured: {exc}')
+        print(f'Error occurred: {exc}')
         return {'result': 'failed', 'reason': 'internal error'}
 
     else:
         return {'result': 'success'}
 
-    finally:
-        await conn.close()
+
+async def check_current_balance(conn, user_id: str, amount: int) -> Tuple[bool, Optional[dict]]:
+    if amount < 0:
+        fetch_result = await conn.fetchrow(f'''SELECT balance FROM customer WHERE userid = '{user_id}\'''')
+        if not fetch_result:
+            return False, {'result': 'failed', 'reason': 'account does not exist'}  # todo: change
+
+        current_balance = fetch_result['balance']
+        to_be = current_balance + amount
+        if to_be < 0:
+            return False, {'result': 'failed', 'reason': 'not enough money'}
+
+    return True, None
--- stub_payment_service/db_service.py
+++ stub_payment_service/db_service.py
@@ -1,6 +1,23 @@
+from typing import Optional
+
 import asyncpg
 
 from .configs.config import get_settings
+
+
+class DBConnectionContext:
+
+    def __init__(self):
+        self.conn: Optional[asyncpg.connection.Connection] = None
+        self._settings = get_settings()
+
+    async def __aenter__(self) -> asyncpg.connection.Connection:
+        self.conn = await asyncpg.connect(user=self._settings.db_user, password=self._settings.db_password,
+                                          database=self._settings.db_name, host=self._settings.db_host, port=self._settings.db_port)
+        return self.conn
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        await self.conn.close()
 
 
 async def get_db_connection() -> asyncpg.connection.Connection:
